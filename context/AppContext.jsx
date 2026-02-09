0cc5878ff82294020716055496328a9a9c2726f5
siddharthatomar33 <siddharthatomar33@gmail.com>
2026-02-09 20:51:05 +0530

    GreenChart

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,9 +1,21 @@
-import { Children, createContext } from "react";
+import { Children, createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider=({children})=>{
-    return <AppContext.Provider>
-        
+
+    const navigate = useNavigate();
+    const[user, setUser]=useState(null)
+    const[isSeller, setIsSeller]=useState(false)
+
+    const value ={navigate, user, setUser,setIsSeller,isSeller}
+
+    return <AppContext.Provider value={value}>
+        {children}
     </AppContext.Provider>
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
 }
