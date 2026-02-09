0a75dc619d45ef985b4755e4777f09da8f615f31
siddharthatomar33 <siddharthatomar33@gmail.com>
2026-02-09 20:15:55 +0530

    GreenChart

---
+++ context/AppContext.jsx
@@ -0,0 +1,9 @@
+import { Children, createContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider=({children})=>{
+    return <AppContext.Provider>
+        
+    </AppContext.Provider>
+}
